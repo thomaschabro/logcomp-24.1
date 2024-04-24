ca2220af3b2392aeabe470580da9a3d5deb0b758
thomaschabro <81168920+thomaschabro@users.noreply.github.com>
2024-04-24 17:55:41 -0300

    aparentemente funcionando

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ class Tokenizer():
             "and",
             "or",
             "not",
-            "read" ]
+            "read",
+            "local"]
     
     def selectNext(self):
         if self.position >= len(self.source):
@@ -73,6 +74,17 @@ class Tokenizer():
             elif self.source[self.position] == "<":
                 self.position += 1
                 self.next = Token("LT", "<")
+            elif self.source[self.position] == "\"":
+                string = ""
+                self.position += 1
+                while self.position < len(self.source) and self.source[self.position] != "\"":
+                    string += self.source[self.position]
+                    self.position += 1
+                if self.position == len(self.source):
+                    sys.stderr.write("Erro de sintaxe. Aspas não fechadas. (1)")
+                    sys.exit(1)
+                self.position += 1
+                self.next = Token("STR", string)
             elif self.source[self.position] == "=":
                 self.position += 1
                 if self.position < len(self.source) and self.source[self.position] == "=":
@@ -80,6 +92,11 @@ class Tokenizer():
                     self.next = Token("EQ", "==")
                 else:
                     self.next = Token("ASSIGN", "=")
+            elif self.source[self.position] == ".":
+                self.position += 1
+                if self.position < len(self.source) and self.source[self.position] == ".":
+                    self.position += 1
+                    self.next = Token("CONCAT", "..")
             elif self.source[self.position].isdigit():
                 number = ""
                 while self.position < len(self.source) and self.source[self.position].isdigit():
@@ -115,43 +132,66 @@ class BinOp(Node):
         super().__init__(value, children)
 
     def Evaluate(self, st):
-        if self.value == "+":
-            return int(self.children[0].Evaluate(st)) + int(self.children[1].Evaluate(st))
-        elif self.value == "-":
-            return int(self.children[0].Evaluate(st)) - int(self.children[1].Evaluate(st))
-        elif self.value == "*":
-            return int(self.children[0].Evaluate(st)) * int(self.children[1].Evaluate(st))
-        elif self.value == "/":
-            return int(self.children[0].Evaluate(st)) // int(self.children[1].Evaluate(st))
-        elif self.value == "==":
-            return int(self.children[0].Evaluate(st)) == int(self.children[1].Evaluate(st))
-        elif self.value == ">":
-            return int(self.children[0].Evaluate(st)) > int(self.children[1].Evaluate(st))
-        elif self.value == "<":
-            return int(self.children[0].Evaluate(st)) < int(self.children[1].Evaluate(st))
-        elif self.value == "and":
-            return int(self.children[0].Evaluate(st)) and int(self.children[1].Evaluate(st))
-        elif self.value == "or":
-            return int(self.children[0].Evaluate(st)) or int(self.children[1].Evaluate(st))
+        print(self.value, self.children[0].Evaluate(st), self.children[1].Evaluate(st))
+        if self.value == "..":
+            return ["STR", str(self.children[0].Evaluate(st)[1]) + str(self.children[1].Evaluate(st)[1])]
+        if self.children[0].Evaluate(st)[0] == "INT" and self.children[1].Evaluate(st)[0] == "INT":    
+            if self.value == "+":
+                return ["INT", int(self.children[0].Evaluate(st)[1]) + int(self.children[1].Evaluate(st)[1])]
+            elif self.value == "-":
+                return ["INT", int(self.children[0].Evaluate(st)[1]) - int(self.children[1].Evaluate(st)[1])]
+            elif self.value == "*":
+                return ["INT", int(self.children[0].Evaluate(st)[1]) * int(self.children[1].Evaluate(st)[1])]
+            elif self.value == "/":
+                return ["INT", int(self.children[0].Evaluate(st)[1]) // int(self.children[1].Evaluate(st)[1])]
+            elif self.value == "and":
+                return ["INT", int(self.children[0].Evaluate(st)[1]) and int(self.children[1].Evaluate(st)[1])]
+            elif self.value == "or":
+                return ["INT", int(self.children[0].Evaluate(st)[1]) or int(self.children[1].Evaluate(st)[1])]
+            elif self.value == "==":
+                return ["INT", int(int(self.children[0].Evaluate(st)[1]) == int(self.children[1].Evaluate(st)[1]))]
+        elif self.children[0].Evaluate(st)[0] == self.children[1].Evaluate(st)[0]:
+            if self.value == "==":
+                return [self.children[0].Evaluate(st)[0], int(int(self.children[0].Evaluate(st)[1]) == int(self.children[1].Evaluate(st)[1]))]
+            elif self.value == ">":
+                return [self.children[0].Evaluate(st)[0], int(int(self.children[0].Evaluate(st)[1]) > int(self.children[1].Evaluate(st)[1]))]
+            elif self.value == "<":
+                return [self.children[0].Evaluate(st)[0], int(int(self.children[0].Evaluate(st)[1]) < int(self.children[1].Evaluate(st)[1]))]
+        else:
+            sys.stderr.write("Tipo dos operadores inválido.")
+            sys.exit(1)
+            return
 
 class UnOp(Node):
     def __init__(self, value, children):
         super().__init__(value, children)
 
     def Evaluate(self, st):
-        if self.value == "+":
-            return int(self.children[0].Evaluate(st))
-        elif self.value == "-":
-            return int(-self.children[0].Evaluate(st))
-        elif self.value == "not":
-            return int(not self.children[0].Evaluate(st))
+        if self.children[0].Evaluate(st)[0] == "INT":
+            if self.value == "+":
+                return ["INT", int(self.children[0].Evaluate(st)[1])]
+            elif self.value == "-":
+                return ["INT", int(-self.children[0].Evaluate(st)[1])]
+            elif self.value == "not":
+                return ["INT", int(not self.children[0].Evaluate(st)[1])]
+        else:
+            sys.stderr.write("Tipo dos operadores inválido para UnOp")
+            sys.exit(1)
+            return
         
 class IntVal(Node):
     def __init__(self, value):
         super().__init__(value, [])
 
     def Evaluate(self, st):
-        return int(self.value)
+        return ["INT", int(self.value)]
+    
+class StrVal(Node):
+    def __init__(self, value):
+        super().__init__(value, [])
+
+    def Evaluate(self, st):
+        return ["STR", self.value]
     
 class NoOp(Node):
     def __init__(self):
@@ -173,21 +213,45 @@ class Identifier(Node):
         super()._init_(value, [])
 
     def Evaluate(self, st):
-        return st.get(self.value)
+        try:
+            try:
+                true = st.get(self.value).isalpha()
+                return ["STR", st.get(self.value)]
+            except:
+                return ["INT", st.get(self.value)]
+        except:
+            sys.stderr.write("Variável sendo usada sem ser declarada [ " + self.value + " ]")
+            sys.exit(1)
 
 class Assign(Node):
     def _init_(self, children):
         super()._init_(None, children)
 
     def Evaluate(self, st):
-        st.set(self.children[0].value, self.children[1].Evaluate(st))
+        st.set(self.children[0].value, self.children[1].Evaluate(st)[1], self.children[1].Evaluate(st)[0])
+
+class VarDec(Node):
+    def _init_(self, children):
+        super()._init_(None, children)
+
+    def Evaluate(self, st):
+        if self.children[1] is not None:
+            st.create(self.children[0].value)
+            st.set(self.children[0].value, self.children[1].Evaluate(st)[1], self.children[1].Evaluate(st)[0])
+        else:
+            st.create(self.children[0].value)   
 
 class Print(Node):
     def _init_(self, children):
         super()._init_(None, children)
 
     def Evaluate(self, st):
-        sys.stdout.write(str(int(self.children[0].Evaluate(st))) + "\n")
+        var = self.children[0].Evaluate(st)
+        print(var)
+        if var[0] == "STR":
+            sys.stdout.write(str(var[1]) + "\n")
+        elif var[0] == "INT":
+            sys.stdout.write(str(var[1]) + "\n")
 
 class If(Node):
     def _init_(self, children):
@@ -213,7 +277,7 @@ class Read(Node):
         super()._init_(None, children)
 
     def Evaluate(self, st):
-        return self.value
+        return ["INT", self.value]
         
 class Parser:
     def __init__(self, tokenizer):
@@ -232,10 +296,17 @@ class Parser:
             elif tok.next is not None and tok.next.type == "MINUS":
                 tok.selectNext()
                 if tok.next.type != "NUMBER" and tok.next.type != "LPAREN" and tok.next.type != "PLUS" and tok.next.type != "MINUS" and tok.next.type != "IDEN":
-                    sys.stderr.write("Erro de sintaxe. Número ou '(' esperado. (6)")
+                    sys.stderr.write("Erro de sintaxe. Número ou '(' esperado. (6 1 )")
                     sys.exit(1)
                 else:
                     resultado = BinOp("-", [resultado, Parser.parseTerm(tok)])
+            elif tok.next is not None and tok.next.type == "CONCAT":
+                tok.selectNext()
+                if tok.next.type != "NUMBER" and tok.next.type != "LPAREN" and tok.next.type != "PLUS" and tok.next.type != "MINUS" and tok.next.type != "IDEN":
+                    sys.stderr.write("Erro de sintaxe. Número ou '(' esperado. (6 2 )")
+                    sys.exit(1)
+                else:
+                    resultado = BinOp("..", [resultado, Parser.parseTerm(tok)])
             else:
                 return resultado
 
@@ -303,6 +374,10 @@ class Parser:
                 else:
                     tok.selectNext()
                     return Read(value=int(input()))
+        elif tok.next.type == "STR":
+            string = tok.next.value
+            tok.selectNext()
+            return StrVal(value=string)
         else:
             sys.stderr.write("Erro de sintaxe. Número ou '(' esperado. (10)")
             sys.exit(1)
@@ -398,6 +473,27 @@ class Parser:
                 sys.exit(1)
             tok.selectNext()
             return While(value="while", children=[condicao, lista_statements])
+        elif tok.next.type == "LOCAL":
+            tok.selectNext()
+            if tok.next.type != "IDEN":
+                sys.stderr.write("Erro de sintaxe. Identificador esperado. (12)")
+                sys.exit(1)
+            iden = tok.next.value
+            tok.selectNext()
+            if tok.next.type == "ASSIGN":
+                tok.selectNext()
+                saida = Parser.parseBoolExp(tok)
+                if tok.next.type != "NL":
+                    sys.stderr.write("Erro de sintaxe. Nova linha esperada após NL. (13)")
+                    sys.exit(1)
+                tok.selectNext()
+                return VarDec(value="assign", children=[Identifier(iden), saida])
+            elif tok.next.type == "NL":
+                tok.selectNext()
+                return VarDec(value="assign", children=[Identifier(iden), None])
+            else:
+                sys.stderr.write("Erro de sintaxe. Erro após LOCAL. (14)")
+                sys.exit(1)
         else:
             sys.stderr.write("Erro de sintaxe. '=' esperado. (3)")
             sys.exit(1)
@@ -460,11 +556,18 @@ class SymbolTable:
     def __init__(self):
         self.table = {}
     
-    def set(self, key, value):
-        self.table[key] = value
-    
+    def set(self, key, value, type):
+        if key in self.table:
+            self.table[key] = [value, type]
+        else:
+            sys.stderr.write("Variável sendo definida sem ser declarada [ " + key + " ]")
+            sys.exit(1)
+
     def get(self, key):
-        return self.table[key]
+        return self.table[key][0]
+    
+    def create(self, key):
+        self.table[key] = None
 
 def main():
     if len(sys.argv) != 2:
