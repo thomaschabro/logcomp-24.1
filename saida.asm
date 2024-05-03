00eb0aa5929b5d2cb59ef2c7faba4dfacdc969bd
Thomas Chabro <tcchabro@gmail.com>
2024-05-03 16:34:08 -0300

    update

--- saida.asm
+++ saida.asm
@@ -6,16 +6,12 @@ STDIN equ 0
 STDOUT equ 1
 True equ 1
 False equ 0
-
 segment .data
-
 formatin: db "%d", 0
 formatout: db "%d", 10, 0 ; newline, nul terminator
 scanint: times 4 db 0 ; 32-bits integer = 4 bytes
-
 segment .bss  ; variaveis
 res RESB 1
-
 section .text
 global main ; linux
 ;global _main ; windows
@@ -27,20 +23,16 @@ extern fflush ; linux
 ;extern _fflush ; windows
 extern stdout ; linux
 ;extern _stdout ; windows
-
 ; subrotinas if/while
 binop_je:
 JE binop_true
 JMP binop_false
-
 binop_jg:
 JG binop_true
 JMP binop_false
-
 binop_jl:
 JL binop_true
 JMP binop_false
-
 binop_false:
 MOV EAX, False  
 JMP binop_exit
@@ -48,25 +40,64 @@ binop_true:
 MOV EAX, True
 binop_exit:
 RET
-
 main:
-
 PUSH EBP ; guarda o base pointer
 MOV EBP, ESP ; estabelece um novo base pointer
 
 ; codigo gerado pelo compilador abaixo
 
-
+PUSH DWORD 0 ;
+MOV EAX, 1
+MOV [EBP-4], EAX ;
+LOOP_1:
+MOV EAX, 5
+PUSH EAX
+MOV EAX, [EBP-4] ;
+POP EBX
+CMP EAX, EBX ;
+SETL AL ;
+MOVZX EAX, AL ;
+CMP EAX, False
+JE END_1
+MOV EAX, [EBP-4] ;
+PUSH EAX
+PUSH formatout
+CALL printf
+ADD ESP, 8
+MOV EAX, 1
+PUSH EAX
+MOV EAX, [EBP-4] ;
+POP EBX
+ADD EAX, EBX ;
+MOV [EBP-4], EAX ;
+IF_1:
+MOV EAX, 3
+PUSH EAX
+MOV EAX, [EBP-4] ;
+POP EBX
+CMP EAX, EBX ;
+SETE AL ;
+MOVZX EAX, AL ;
+CMP EAX, False
+JE ELSE_1
+MOV EAX, 1010
+PUSH EAX
+PUSH formatout
+CALL printf
+ADD ESP, 8
+JMP ENDIF_1
+ELSE_1:
+ENDIF_1:
+JMP LOOP_1
+END_1:
 
 ; interrupcao de saida (default)
 
 PUSH DWORD [stdout]
 CALL fflush
 ADD ESP, 4
-
 MOV ESP, EBP
 POP EBP
-
 MOV EAX, 1
 XOR EBX, EBX
 INT 0x80
--- teste.lua
+++ teste.lua
@@ -1,6 +1,9 @@
 local x
 x = 1
 while x < 5 do
-  	x = x + 1
-	print(x)
+  print(x)
+  x = x + 1
+  if x == 3 then
+	print(1010)
+  end
 end
